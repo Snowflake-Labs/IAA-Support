cab9e7e938f7fdaa9d6e43c3b67fa454eb31a365
David Madriz Villagra <david.madrizvillagra@snowflake.com>
2025-03-17 16:38:39 -0600

    Upload top 3 mappings

--- deployment/upload_mappings.py
+++ deployment/upload_mappings.py
@@ -1,18 +1,23 @@
 import os
 import subprocess
-
+from packaging.version import Version
 
 def upload_mappings(folder_path):
     if not os.path.exists(folder_path):
         raise ValueError(f"The folder {folder_path} does not exist.")
 
-    for version_folder in os.listdir(folder_path):
+    version_folders = os.listdir(folder_path)
+    sorted_versions = sorted(version_folders, key=Version, reverse=True)
+    version_processed = 0
+    for version_folder in sorted_versions:
         version_folder_path = os.path.join(folder_path, version_folder)
 
         if not os.path.isdir(version_folder_path):
             continue
 
         print(f"Processing version: {version_folder}")
+        if version_processed == 3:
+            break
 
         for type_folder in os.listdir(version_folder_path):
             type_folder_path = os.path.join(version_folder_path, type_folder)
@@ -39,6 +44,7 @@ def upload_mappings(folder_path):
                     check=True,
                 )
                 print(f"Successfully uploaded files for {version_folder}/{type_folder}")
+                version_processed += 1
             except subprocess.CalledProcessError as e:
                 print(f"Error uploading files for {version_folder}/{type_folder}: {e}")
 
